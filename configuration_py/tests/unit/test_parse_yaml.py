c41c3c19a337655599fddd211a8abd5a5efdffca
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2017-03-25 23:31:41 +0200

    Shorten test names

--- configuration_py/tests/unit/test_parse_yaml.py
+++ configuration_py/tests/unit/test_parse_yaml.py
@@ -4,17 +4,17 @@ from configuration_py.configuration_py import _parse_yaml
 
 
 class TestParseYAML(TestCase):
-    def test_parse_yaml_should_return_dict_of_parsed_yaml_content(self):
+    def test_should_return_dict_of_parsed_yaml_content(self):
         content = """ development:
                         variable: True"""
         expected_result = {'development': {'variable': True}}
         actual_result = _parse_yaml(content)
         self.assertDictEqual(expected_result, actual_result)
 
-    def test_parse_yaml_should_raise_exception_if_no_environment_variables_provided(self):
+    def test_should_raise_exception_if_no_environment_variables_provided(self):
         content = """ development """
         self.assertRaises(EnvironmentError, _parse_yaml, content)
 
-    def test_parse_yaml_should_raise_exception_if_no_content_provided(self):
+    def test_should_raise_exception_if_no_content_provided(self):
         content = ""
         self.assertRaises(EnvironmentError, _parse_yaml, content)
--- configuration_py/tests/unit/test_read_config_file.py
+++ configuration_py/tests/unit/test_read_config_file.py
@@ -5,7 +5,7 @@ from mock import patch, mock_open
 
 
 class TestReadConfigFile(TestCase):
-    def test_read_config_file_should_return_file_content(self):
+    def test_should_return_file_content(self):
         expected_data = "data"
         with patch("__builtin__.open", mock_open(read_data="data")) as mock_file:
             actual_data = _read_config_file('/path/to/config.yaml')
