b2e76d3e639d3fdf5dd54072b1e2633afca7672a
akozlovskyi <sam.to.life@gmail.com>
2017-04-20 15:22:46 +0300

    Fix idiomatic typecheck

--- configuration_py/parsers/yaml_parser.py
+++ configuration_py/parsers/yaml_parser.py
@@ -9,6 +9,6 @@ class YAMLParser(BaseConfigParser):
 
     def parse(self, file_content, context={}):
         config_dict = yaml.load(file_content)
-        if not config_dict or type(config_dict) is not dict:
+        if not config_dict or not isinstance(config_dict, dict):
             raise EnvironmentError('Config file does not contain config variables')
         return config_dict
